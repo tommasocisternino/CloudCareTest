e171f21b0dcf33e829e514a3a3a0a79e8402a5bc
tommaso.cisternino <tommycisternino@gmail.com>
2023-08-27 20:41:58 +0200

    created login.blade.php and route in web.php. implemented login. some style in app.css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,33 +11,50 @@
         </label>
         <button id="login-button" type="submit">LOGIN</button>
     </form>
+
+    <div class="hidden login-status-div">
+        <label class="login-status-label"></label>
+    </div>
 </div>
 
 <script>
-    document.getElementById('login-form').addEventListener('submit', function (e) {
+    localStorage.removeItem("user_info");
+
+    const loginStatusDiv = document.querySelector('.login-status-div');
+    const loginStatusLabel = document.querySelector('.login-status-label');
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+
+    document.getElementById('login-form').addEventListener('submit', (e) => {
         e.preventDefault();
         login();
     });
 
-    function login (){
+    function login() {
         axios.post('/login', {
-            username: document.getElementById('username').value,
-            password: document.getElementById('password').value,
+            username: usernameInput.value,
+            password: passwordInput.value,
         }, {
             headers: {
                 'X-CSRF-TOKEN': "{{ csrf_token() }}"
             }
         })
-            .then(function (response) {
-                // handle success
-                console.log(response);
+            .then((response) => {
+                if (response.status === 200) {
+                    localStorage.setItem("user_info", JSON.stringify(response.data));
+                    setLoginStatus("login-status-success-div login-status-div", "Login effettuato con successo, stai per essere reindirizzato alla pagina principale");
+                    setTimeout(() => {
+                        location.href = "/";
+                    }, 3000)
+                }
             })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .finally(function () {
-                // always executed
+            .catch((error) => {
+                setLoginStatus("login-status-error-div login-status-div", "Login fallito, credenziali errate");
             });
+    }
+
+    function setLoginStatus(className, text) {
+        loginStatusDiv.className = className;
+        loginStatusLabel.textContent = text;
     }
 </script>
--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,12 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
-})->name('home')->middleware('auth');
-gi
-Route::post('login', [AuthController::class, 'login'])->name('login');
+    return view('home');
+})->name('home')->middleware('jwtcookie.auth');
 
 Route::get('login', function () {
     return view("auth.login");
-})->middleware("guest")->name('login-view');
+})->middleware("jwtcookie.guest")->name('login-view');
 
+Route::post('login', [AuthController::class, 'login'])->name('login');
