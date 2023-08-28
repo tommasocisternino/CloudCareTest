4c39ecb63fb6cf1e69b99fa711502bc21cb3472d
tommycisternino <tommycisternino@gmail.com>
2023-08-28 21:09:45 +0200

    DataTable for BeersList

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,58 @@
 @extends("layouts/base")
 @section('head')
     @parent
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <link href="https://cdn.datatables.net/v/bs5/dt-1.13.6/b-2.4.1/datatables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet">
+    <script src="https://cdn.datatables.net/v/bs5/dt-1.13.6/b-2.4.1/datatables.min.js"></script>
 @endsection
-<div>
-    <button id="login-button" type="submit">ASDASDSADASDASDAS</button>
-</div>
 
-<script src="{{ asset("/js/services/beers.js") }}"></script>
-<script type="module">
-    document.getElementById('login-button').addEventListener('click', (e) => {
-        check();
-    });
+@section("content")
+    <div class="home-page overflow-hidden">
+        <label class="fw-bolder w-100 text-center">Ecco il tuo access_token!</label>
+        <div class="fw-bolder mb-5 token-div"><p id="access_token_p"></p></div>
+        <table id="data" class="display" style="width:100%">
+            <thead>
+            <tr>
+                <th>Nome birra</th>
+                <th>Tagline</th>
+                <th>Prima produzione</th>
+            </tr>
+            </thead>
+            <tfoot>
+            <tr>
+                <th>Nome birra</th>
+                <th>Tagline</th>
+                <th>Prima produzione</th>
+            </tr>
+            </tfoot>
+        </table>
+    </div>
+@endsection
 
-    function check() {
-        BeerService.check()
-            .then((response) => {
-                console.log(response)
-            })
-            .catch((error) => {
-                console.log(error)
-            });
-    }
-</script>
+@section("scripts")
+    <script src="{{ asset("/js/services/beers.js") }}"></script>
+    <script type="module">
+        document.getElementById('access_token_p').textContent = USER_INFO.access_token;
+        let table = new DataTable('#data', {
+            ajax: {
+                url: "{{ route("get-beers-list") }}",
+                dataSrc: 'data',
+                lengthChange: true,
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', 'Bearer ' + USER_INFO.access_token);
+                }
+            },
+            pagination: true,
+            processing: true,
+            serverSide: true,
+            searching:false,
+            "lengthMenu": [10, 25, 50, 75],
+            columns: [
+                {data: 'name',orderable: false },
+                {data: 'tagline',orderable: false },
+                {data: 'first_brewed',orderable: false },
+            ]
+        });
+    </script>
+@endsection
